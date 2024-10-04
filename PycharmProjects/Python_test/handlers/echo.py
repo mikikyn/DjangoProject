ece9c2045cb1439f967909e997523827f75aedd5
Baizhigit Akaev <78411819+mikikyn@users.noreply.github.com>
2024-10-04 18:46:31 +0600

    Delete PycharmProjects directory

--- PycharmProjects/Python_test/handlers/echo.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types, Dispatcher
-
-
-async def echo_handler(message: types.Message):
-    await message.answer(message.text)
-
-
-def register_echo(dp: Dispatcher):
-    dp.register_message_handler(echo_handler)
